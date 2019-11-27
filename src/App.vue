f9201db73526b9b63b5fb8e50757b2a54fcad000
JunHoPark <pjh374968@gmail.com>
2019-11-27 16:26:18 +0900

    v-for을 이용한 렌더링

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,49 @@
 <template> 
-  <div>
-    <Header/>
-    <div class="wrap">
-      <Menu/>
-      <Content/>
-    </div>
-    <Box color="blue"/>
-    <Box color="purple"/>
-    <Box color="green"/>
-    <Box color="blue"/>
-    <Box color="purple"/>
-    <Box />
+  <div id="app">
+    <Webtoon v-bind:items="webtoons"/>
   </div>
 </template>
 
 <script>  
-import Content from "./components/Content";
-import Menu from "./components/Menu";
-import Header from "./components/Header";
-import Box from "./components/Box";
+import Webtoon from "./components/Webtoon";
 
 export default {
   name: "app",
   components: {
-    Content,
-    Menu,
-    Header,
-    Box
+    Webtoon
+  },
+  data() {
+    return {
+      webtoons: [
+        {
+          name: "햄스터와 그녀",
+          link: "http://webtoon.daum.net/webtoon/view/hamsterandher",
+          img:
+            "http://t1.daumcdn.net/webtoon/op/478cdf37f585607982ffa9e35b432e8503be8a54"
+        },
+        {
+          name: "프롬 스타",
+          link: "http://webtoon.daum.net/webtoon/view/fromstar",
+          img:
+            "http://t1.daumcdn.net/webtoon/op/a7fb953d722c1130bfc18440f7e3ce448ece57a1"
+        },
+        {
+          name: "위대한 로맨스",
+          link: "http://webtoon.daum.net/webtoon/view/greatromance",
+          img:
+            "http://t1.daumcdn.net/webtoon/op/a816281cb4df5c50a20ac386fd6e496643d0f085"
+        },
+        {
+          name: "빛나는 손을",
+          link: "http://webtoon.daum.net/webtoon/view/Hand",
+          img: "http://t1.daumcdn.net/cartoon/5913FCAC0234C50001"
+        }
+      ]
+    };
   }
 };
 </script>
 
 <style>   
-  h1 {
-    color: #03a9f4;
-  }
 
-  .wrap {
-    display: flex
-  }
 </style>
