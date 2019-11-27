7ec497b85c6d8f89a5a7633f9250b7d198572258
JunHoPark <pjh374968@gmail.com>
2019-11-27 17:09:33 +0900

    vue-router

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,12 @@
 <template>
   <div id="app">
-    {{ message }}
-    <h2>뒤집힌</h2>
-    {{ reversedMessage }}
+        <router-view/>
   </div>  
 </template>
 
 <script>
 
 export default {
-  name: 'app',
-  data () {
-    return {
-      message: 'Hello Vue !!'
-    }
-  },
-  // computed는 대상이 변하지 않으면 계산하지 않고 캐싱을 이용한다.
-  // method: {} 는 캐싱 이득을 취할 수 없다.
-  computed: {
-    reversedMessage() {
-      return this.message.split("").reverse().join("");
-    }
-  }
+  name: 'app'
 }
 </script>
