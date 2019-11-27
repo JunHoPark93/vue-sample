25f87cefc3f7ef98640d71580e899a596ca2adc9
JunHoPark <pjh374968@gmail.com>
2019-11-27 16:50:53 +0900

    vue-computed

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,26 @@
-<template> 
+<template>
   <div id="app">
-    <h2>선호 장르</h2>
-    <select v-model="category">
-      <option disabled value>선택해주세요</option>
-      <option>로맨스</option>
-      <option>호러</option>
-      <option>스릴러</option>
-    </select>
-    <span>선택: {{ category }}</span>
-  </div>
+    {{ message }}
+    <h2>뒤집힌</h2>
+    {{ reversedMessage }}
+  </div>  
 </template>
 
-<script>  
+<script>
 
 export default {
-  name: "app",
-  data() {
+  name: 'app',
+  data () {
     return {
-      title: "",
-      category: [],
-    };
+      message: 'Hello Vue !!'
+    }
+  },
+  // computed는 대상이 변하지 않으면 계산하지 않고 캐싱을 이용한다.
+  // method: {} 는 캐싱 이득을 취할 수 없다.
+  computed: {
+    reversedMessage() {
+      return this.message.split("").reverse().join("");
+    }
   }
-};
+}
 </script>
-
-<style>   
-
-</style>
